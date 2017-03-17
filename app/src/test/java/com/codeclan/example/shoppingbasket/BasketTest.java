1a2ebf54bb33da33e13b76633af5685e2b396f6a
Colin McKean <colinmckeandigital@gmail.com>
2017-03-17 18:53:42 +0000

    add test that adding items to basket increases count

--- app/src/test/java/com/codeclan/example/shoppingbasket/BasketTest.java
+++ app/src/test/java/com/codeclan/example/shoppingbasket/BasketTest.java
@@ -11,19 +11,26 @@ import static org.junit.Assert.assertEquals;
 
 public class BasketTest {
     Basket basket;
+    ShoppingItem item;
 
     @Before
     public void before() {
         basket = new Basket();
+        item = new ShoppingItem("eggs", 15.99f);
     }
 
     @Test
     public void testEmptyBasketTotal() {
-        assertEquals(0, basket.getTotal());
+        assertEquals(0, basket.getTotal(), 0.01);
     }
 
     @Test
     public void testNumOfItemsInEmptyBasket() {
         assertEquals(0, basket.getNumOfItemsOfBasket());
+    }
+    @Test
+    public void testAddItemToBasketIncreasesCount(){
+        basket.addToBasket(item);
+        assertEquals(1, basket.getNumOfItemsOfBasket());
     }
 }
