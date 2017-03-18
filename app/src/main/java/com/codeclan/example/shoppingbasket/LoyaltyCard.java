6c51c29d0d5d11b981cf39a19eea06d58a9add96
Colin McKean <colinmckeandigital@gmail.com>
2017-03-18 20:41:31 +0000

    add loyalty card class

---
+++ app/src/main/java/com/codeclan/example/shoppingbasket/LoyaltyCard.java
@@ -0,0 +1,17 @@
+package com.codeclan.example.shoppingbasket;
+
+/**
+ * Created by user on 18/03/2017.
+ */
+
+class LoyaltyCard {
+    private double discountRate;
+
+    public LoyaltyCard(double discountRate) {
+        this.discountRate = discountRate;
+
+    }
+    public double getDiscountRate() {
+        return discountRate;
+    }
+}
