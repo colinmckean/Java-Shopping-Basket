8015eac1e629fa1d2c1cb0673951ba1197a84402
Colin McKean <colinmckeandigital@gmail.com>
2017-03-18 17:48:17 +0000

    add checkouttest to ensure can get buyone getone free.

--- app/src/test/java/com/codeclan/example/shoppingbasket/CheckoutTest.java
+++ app/src/test/java/com/codeclan/example/shoppingbasket/CheckoutTest.java
@@ -12,23 +12,36 @@ import static org.junit.Assert.assertEquals;
 public class CheckoutTest {
     Customer nonLoyalCustomer;
     Customer loyalCustomer;
+    LoyaltyCard loyaltyCard;
     Basket basket;
     ShoppingItem beans;
     ShoppingItem newsPaper;
+    ShoppingItem cheese;
+    ShoppingItem hotdogs;
     @Before
     public void before(){
         nonLoyalCustomer = new Customer();
-        loyalCustomer = new Customer();
+        loyaltyCard = new LoyaltyCard(2);
+        loyalCustomer = new Customer(loyaltyCard);
         basket = new Basket();
         beans = new ShoppingItem("beans", 19.50);
         newsPaper = new ShoppingItem("beans", 19.50);
+        cheese = new ShoppingItem("cheese", 3.99);
+        hotdogs = new ShoppingItem("hotdogs", 6.99);
+
+
     }
 
     @Test
     public void testSingleItemUnderSpendNonLoyalCustomer(){
         basket.addToBasket(beans);
-        Checkout.getTotal(nonLoyalCustomer, basket);
-        assertEquals(19,50, Checkout.getTotal(nonLoyalCustomer, basket));
+        assertEquals(19.50, Checkout.getTotal(nonLoyalCustomer, basket), 0.01);
     }
 
+    @Test
+    public void testMultiBuyDiscount(){
+        basket.addToBasket(cheese);
+        basket.addToBasket(cheese);
+        assertEquals(3.99, Checkout.getTotal(nonLoyalCustomer, basket), 0.01);
+    }
 }
