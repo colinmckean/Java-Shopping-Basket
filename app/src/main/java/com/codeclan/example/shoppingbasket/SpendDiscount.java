a54a9dbe81112d3a47ac3b542f836da14e4edb0b
Colin McKean <colinmckeandigital@gmail.com>
2017-03-19 10:31:55 +0000

    add discount to apply if over spend

---
+++ app/src/main/java/com/codeclan/example/shoppingbasket/SpendDiscount.java
@@ -0,0 +1,15 @@
+package com.codeclan.example.shoppingbasket;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 18/03/2017.
+ */
+
+public class SpendDiscount {
+
+    public static double calculateDiscount(double subTotal) {
+        return subTotal > 20.00 ? subTotal / 10.00 : 0.0;
+    }
+}
+
